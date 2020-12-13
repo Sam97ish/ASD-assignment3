b2c3060a53ffd34e87ae0bd3c0277686f217cb40
Findlay <Findlay12@ymail.com>
2020-12-13 17:08:51 +0100

    changed return type of array to string

--- src/main/assignment3/impl/main.java
+++ src/main/assignment3/impl/main.java
@@ -170,29 +170,32 @@ public class main {
 
 
 
-       MyUndirectedUnweightedGraphImpl<Integer> undirectedGraph = new MyUndirectedUnweightedGraphImpl<>();
+       MyUndirectedUnweightedGraphImpl <String> undirectedGraph = new MyUndirectedUnweightedGraphImpl<>();
 
-       undirectedGraph.addVertex(1);
-       undirectedGraph.addVertex(2);
-       undirectedGraph.addVertex(3);
-       undirectedGraph.addVertex(4);
-       undirectedGraph.addVertex(5);
+       undirectedGraph.addVertex("1");
+       undirectedGraph.addVertex("2");
+       undirectedGraph.addVertex("3");
+       undirectedGraph.addVertex("4");
+       undirectedGraph.addVertex("5");
        //undirectedGraph.addVertex(8);
 
-       undirectedGraph.addEdge(1,2);
-       undirectedGraph.addEdge(1,4);
-       undirectedGraph.addEdge(1,3);
-       undirectedGraph.addEdge(1,5);
+       undirectedGraph.addEdge("1","2");
+       undirectedGraph.addEdge("1","4");
+       undirectedGraph.addEdge("1","3");
+       undirectedGraph.addEdge("1","5");
 
-       undirectedGraph.addEdge(3,4);
-       undirectedGraph.addEdge(3,2);
-       undirectedGraph.addEdge(2,4);
-       undirectedGraph.addEdge(3,5);
+       undirectedGraph.addEdge("3","4");
+       undirectedGraph.addEdge("3","2");
+       undirectedGraph.addEdge("2","4");
+       undirectedGraph.addEdge("3","5");
 
 
        System.out.println(undirectedGraph);
        System.out.println(undirectedGraph.hasEulerPath());
        System.out.println(undirectedGraph.eulerPath());
+       String lol = undirectedGraph.eulerPath().get(0);
+       System.out.println(lol);
+
 
 
 
